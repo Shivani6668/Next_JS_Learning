b6063395402fb3792472468026418e974cdc0ed8
Shivani6668 <shivaniparmar316@gmail.com>
2025-06-02 16:39:33 +0530

    Reusable Layout

---
+++ First_Next_App/app/services/layout.js
@@ -0,0 +1,10 @@
+export default function ServicelAYOUT({children}) {
+  return (
+    <>
+    <section style={{background:"lightblue"}}>
+     <h3>Services Layout</h3>
+      {children}
+     </section>
+  </>
+  )
+}
